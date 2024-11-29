2506c4542f71885e46b2af9b9e5b8b63e7a37ac4
Axel Álvarez <axelalvarezsantos@gmail.com>
2024-11-29 23:01:21 +0100

    third exercise completed

--- src/main/java/ejercicios/UD03TareaEvaluativaEjercicio3.java
+++ src/main/java/ejercicios/UD03TareaEvaluativaEjercicio3.java
@@ -8,6 +8,7 @@ package ejercicios;
 
 import java.sql.*;
 
+// Programa para gestionar asistentes y eventos en una base de datos
 public class UD03TareaEvaluativaEjercicio3 {
     
     // Configuración para la conexión a la base de datos
@@ -21,64 +22,67 @@ public class UD03TareaEvaluativaEjercicio3 {
         // Conexión a la base de datos y flujo general del programa
         try (Connection conn = DriverManager.getConnection(CONNECTION_URL, USERNAME, PASSWORD)) {
             
-            // Obtiene y registra el asistente si no existe
-            String dniAttendee = getOrRegisterAtendee(conn);
+            // Obtiene o registra el asistente si no existe
+            String attendeeDni = getOrRegisterAttendee(conn);
             
-            // Muestra los eventos disponibles y selecciona uno
-            int selectedEvent = selectEvent(conn);
+            // Muestra la lista de eventos
+            printEventList(conn);
+            
+            // Solicita y VALIDA el evento seleccionado por el usuario
+            int selectedEventId = selectAndValidateEvent(conn);
             
             // Registra al asistente en el evento seleccionado
-            registerAttendeeToEvent(conn, dniAttendee, selectedEvent);
+            registerAttendeeToEvent(conn, attendeeDni, selectedEventId);
             
         } catch (SQLException es) {
-            muestraErrorSQL(es);
+            showSQLError(es);
         } catch (Exception e) {
             e.printStackTrace(System.err);
         }
     }
     
     /**
-     * Obtiene el DNI del asistente y verifica si ya está registrado
+     * Solicita el DNI del asistente y verifica si ya está registrado
      * Si no está registrado, solicita el nombre y lo añade a la base de datos
      * @param conn Conexión a la base de datos
      * @return DNI del asistente
      */
-    private static String getOrRegisterAtendee(Connection conn) {
-        String dniAttendee;
-        boolean dniValid = false;
+    private static String getOrRegisterAttendee(Connection conn) {
+        String attendeeDni;
+        boolean isDniValid = false;
         
         do {
             System.out.println("Introduce el DNI del asistente:");
-            dniAttendee = Console.readString().toUpperCase();
-            dniValid = isDniValid(dniAttendee);
+            attendeeDni = Console.readString().toUpperCase();
+            isDniValid = validateDniFormat(attendeeDni);
             
-            if (!dniValid) {
+            if (!isDniValid) {
                 System.out.println("El DNI introducido no es válido. Inténtalo de nuevo.");
             }
-        } while (!dniValid);
+        } while (!isDniValid);
         
         // Intenta buscar al asistente en la base de datos
-        String name = getAttendeeNameByDni(conn, dniAttendee);
+        String attendeeName = findAttendeeNameByDni(conn, attendeeDni);
         
         // Si no lo encuentra, solicita los datos y lo registra
-        if (name == null) {
+        if (attendeeName == null) {
             System.out.println("No se encontró un asistente con el DNI proporcionado.");
             System.out.println("Introduce el nombre del asistente:");
-            name = Console.readString();
-            insertNewAteendee(conn, dniAttendee, name);
+            attendeeName = Console.readString();
+            registerNewAttendee(conn, attendeeDni, attendeeName);
         }
         
-        System.out.println("Estás realizando la reserva para: " + name);
-        return dniAttendee;
+        System.out.println("Estás realizando la reserva para: " + attendeeName);
+        return attendeeDni;
     }
     
     /**
-     * Valida que el DNI introducido tenga el formato correcto
+     * Validación del formato del DNI introducido
      * El formato válido es 8 dígitos seguidos de una letra mayúscula
      * @param dni DNI a validar
      * @return true si el formato es correcto, false en caso contrario
      */
-    private static boolean isDniValid(String dni) {
+    private static boolean validateDniFormat(String dni) {
         return dni != null && dni.matches("\\d{8}[A-Z]");
     }
     
@@ -86,9 +90,9 @@ public class UD03TareaEvaluativaEjercicio3 {
      * Busca el nombre de un asistente en la base de datos por su DNI
      * @param conn Conexión a la base de datos
      * @param dni DNI del asistente
-     * @return Nombre del asistente o null si no existe
+     * @return El nombre del asistente si existe, null si no está registrado
      */
-    private static String getAttendeeNameByDni(Connection conn, String dni) {
+    private static String findAttendeeNameByDni(Connection conn, String dni) {
         String query = "SELECT nombre FROM asistentes WHERE dni = ?";
         
         try (PreparedStatement stmt = conn.prepareStatement(query)) {
@@ -102,7 +106,7 @@ public class UD03TareaEvaluativaEjercicio3 {
             }
             
         } catch (SQLException es) {
-            muestraErrorSQL(es);
+            showSQLError(es);
         } catch (Exception e) {
             e.printStackTrace(System.err);
         }
@@ -111,102 +115,155 @@ public class UD03TareaEvaluativaEjercicio3 {
     }
     
     /**
-     * Registra un nuevo asistente en la base de datos
+     * Inserta un nuevo asistente en la base de datos
      * @param conn Conexión a la base de datos
      * @param dni DNI del asistente
      * @param name Nombre del asistente
      */
-    private static void insertNewAteendee(Connection conn, String dni, String name) {
+    private static void registerNewAttendee(Connection conn, String dni, String name) {
         String query = "INSERT INTO asistentes (dni, nombre) VALUES (?, ?)";
         
         try (PreparedStatement stmt = conn.prepareStatement(query)) {
             stmt.setString(1, dni);
             stmt.setString(2, name);
             stmt.executeUpdate();
-            //debug
-            System.out.println("El asistente " + name + " ha sido insertado.");
+            //System.out.println("El asistente " + name + " ha sido insertado.");
             
         } catch (SQLException es) {
-            muestraErrorSQL(es);
+            showSQLError(es);
         } catch (Exception e) {
             e.printStackTrace(System.err);
         }
     }
     
     /**
-     * Muestra todos los eventos disponibles y permite seleccionar uno
+     * Muestra la lista de eventos junto con los espacios disponibles
      * @param conn Conexión a la base de datos
-     * @return ID del evento seleccionado
      */
-    private static int selectEvent(Connection conn) {
+    private static void printEventList(Connection conn) {
         String query = "SELECT id_evento, nombre_evento, id_ubicacion FROM eventos";
         
-        System.out.println("Lista de eventos:");
-        try (PreparedStatement stmt = conn.prepareStatement(query)) {
-            
-            try (ResultSet rs = stmt.executeQuery()) {
-                while (rs.next()) {
-                    int idEvent = rs.getInt("id_evento");
-                    String nameEvent = rs.getString("nombre_evento");
-                    int idLocation = rs.getInt("id_ubicacion");
-                    int availableSeats = getAvailableSeats(conn, idEvent, idLocation);
-                    
-                    // Muestra los detalles del evento, incluyendo el espacio disponible
-                    System.out.println(idEvent + ". " + nameEvent + " - Espacios disponibles: " + availableSeats);
-                }
+        try (
+                PreparedStatement stmt = conn.prepareStatement(query);
+                ResultSet rs = stmt.executeQuery()
+        ) {
+            // Itera en cada evento para mostrar los detalles
+            System.out.println("Lista de eventos:");
+            while (rs.next()) {
+                int eventId = rs.getInt("id_evento");
+                String eventName = rs.getString("nombre_evento");
+                int locationId = rs.getInt("id_ubicacion");
+                int availableSpaces = calculateAvailableSpaces(conn, eventId, locationId);
                 
-                // Solicita al usuario seleccionar un evento
-                System.out.println("Elige el número del evento al que quiere asistir:");
-                return Console.readInt();
+                // Muestra los detalles del evento, incluyendo los espacios disponible
+                System.out.println(eventId + ". " + eventName + " - Espacios disponibles: " + availableSpaces);
             }
             
         } catch (SQLException es) {
-            muestraErrorSQL(es);
+            showSQLError(es);
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+        }
+    }
+    
+    /**
+     * Solicita al usuario seleccionar un evento y comprueba si tiene espacios disponibles
+     * @param conn Conexión a la base de datos
+     * @return El ID del evento seleccionado
+     */
+    private static int selectAndValidateEvent(Connection conn) {
+        int selectedEventId = -1;
+        boolean isSelectionValid = false;
+        
+        do {
+            
+            System.out.println("Elige el número del evento al que quiere asistir:");
+            selectedEventId = Console.readInt();
+            
+            // Obtiene el ID de la ubicación
+            int locationId = getEventLocationId(conn, selectedEventId);
+            
+            // Comprueba si existen espacios disponibles en la ubicación del evento seleccionado
+            int availableSpaces = calculateAvailableSpaces(conn, locationId, selectedEventId);
+            
+            if (availableSpaces > 0) {
+                isSelectionValid = true;
+            } else {
+                System.out.println("El evento seleccionado está lleno. Por favor, elige otro evento.");
+            }
+            
+        } while (!isSelectionValid);
+        
+        return selectedEventId;
+    }
+    
+    /**
+     * Obtiene el ID de la ubicación de un evento por su ID
+     * @param conn Conexión a la base de datos
+     * @param idEvent ID del evento
+     * @return El ID de la ubicación
+     */
+    private static int getEventLocationId(Connection conn, int idEvent) {
+        String query = "SELECT id_ubicacion FROM eventos WHERE id_evento = ?";
+        
+        try (PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setInt(1, idEvent);
+            
+            // Si obtiene resultados, retorna el id de la ubicación
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getInt("id_ubicacion");
+                }
+            }
+            
+        } catch (SQLException es) {
+            showSQLError(es);
         } catch (Exception e) {
             e.printStackTrace(System.err);
         }
         
-        return -1;
+        return -1; // Retorna -1 si no se encuentra la ubicación
     }
     
     /**
      * Calcula el número de espacios disponibles para un evento
      * @param conn Conexión a la base de datos
-     * @param idEvent ID del evento
-     * @param idLocation ID de la ubicación
+     * @param eventId ID del evento
+     * @param locationId ID de la ubicación
      * @return número de espacios disponibles
      */
-    private static int getAvailableSeats(Connection conn, int idEvent, int idLocation) {
+    private static int calculateAvailableSpaces(Connection conn, int eventId, int locationId) {
         String getEventAttendees = "SELECT COUNT(*) FROM asistentes_eventos WHERE id_evento = ?";
-        int eventAttendees = 0;
-        int eventCapacity = 0;
+        int registeredAttendeesCount = 0;
+        int locationCapacity = 0;
+        int availableSpaces = 0;
         
         try (PreparedStatement stmt = conn.prepareStatement(getEventAttendees)) {
-            stmt.setInt(1, idEvent);
+            stmt.setInt(1, eventId);
             
             // Obtiene los sitios disponibles con la diferencia de la capacidad total y los asistentes registrados
             try (ResultSet rs = stmt.executeQuery()) {
                 if (rs.next()) {
-                    eventAttendees = rs.getInt(1);
-                    eventCapacity = getEventCapacity(conn, idLocation);
-                    return eventCapacity - eventAttendees;
+                    registeredAttendeesCount = rs.getInt(1);
+                    locationCapacity = getEventCapacity(conn, locationId);
+                    availableSpaces = locationCapacity - registeredAttendeesCount;
                 }
             }
             
         } catch (SQLException es) {
-            muestraErrorSQL(es);
+            showSQLError(es);
         } catch (Exception e) {
             e.printStackTrace(System.err);
         }
         
-        return 0;
+        return availableSpaces;
     }
     
     /**
      * Obtiene la capacidad máxima de una ubicación de un evento
      * @param conn Conexión a la base de datos
      * @param idLocation ID de la ubicación
-     * @return Capacidad máxima
+     * @return La capacidad máxima
      */
     private static int getEventCapacity(Connection conn, int idLocation) {
         String getEventCapacityQuery = "SELECT capacidad FROM ubicaciones WHERE id_ubicacion = ?";
@@ -214,7 +271,7 @@ public class UD03TareaEvaluativaEjercicio3 {
         try (PreparedStatement stmt = conn.prepareStatement(getEventCapacityQuery)) {
             stmt.setInt(1, idLocation);
             
-            // obtiene y devuelve la capacidad máxima
+            // Obtiene y devuelve la capacidad máxima
             try (ResultSet rs = stmt.executeQuery()) {
                 if (rs.next()) {
                     return rs.getInt("capacidad");
@@ -222,7 +279,7 @@ public class UD03TareaEvaluativaEjercicio3 {
             }
             
         } catch (SQLException es) {
-            muestraErrorSQL(es);
+            showSQLError(es);
         } catch (Exception e) {
             e.printStackTrace(System.err);
         }
@@ -233,13 +290,13 @@ public class UD03TareaEvaluativaEjercicio3 {
     /**
      * Registra un asistente en un evento si no está ya registrado
      * @param conn Conexión a la base de datos
-     * @param dni DNI del asistente
-     * @param idEvent ID del evento
+     * @param attendeeDni DNI del asistente
+     * @param eventId ID del evento
      */
-    private static void registerAttendeeToEvent(Connection conn, String dni, int idEvent) {
-        String nameAttendee = getAttendeeNameByDni(conn, dni);
-        if (isAttendeeAlReadyRegistered(conn, dni, idEvent)) {
-            System.out.println("El asistente " + nameAttendee + " ya estaba registrado.");
+    private static void registerAttendeeToEvent(Connection conn, String attendeeDni, int eventId) {
+        String attendeeName = findAttendeeNameByDni(conn, attendeeDni);
+        if (isAttendeeAlReadyRegistered(conn, attendeeDni, eventId)) {
+            System.out.println("El asistente " + attendeeName + " ya estaba registrado.");
             return;
         }
         
@@ -247,16 +304,16 @@ public class UD03TareaEvaluativaEjercicio3 {
         int rowsAffected = 0;
         String query = "INSERT INTO asistentes_eventos (dni, id_evento) VALUES (?, ?)";
         try (PreparedStatement stmt = conn.prepareStatement(query)) {
-            stmt.setString(1, dni);
-            stmt.setInt(2, idEvent);
+            stmt.setString(1, attendeeDni);
+            stmt.setInt(2, eventId);
             rowsAffected = stmt.executeUpdate();
             
             if (rowsAffected > 0) {
-                System.out.println(nameAttendee + " ha sido registrado para el evento seleccionado.");
+                System.out.println(attendeeName + " ha sido registrado para el evento seleccionado.");
             }
             
         } catch (SQLException es) {
-            muestraErrorSQL(es);
+            showSQLError(es);
         } catch (Exception e) {
             e.printStackTrace(System.err);
         }
@@ -277,11 +334,11 @@ public class UD03TareaEvaluativaEjercicio3 {
             stmt.setInt(2, idEvent);
             
             try (ResultSet rs = stmt.executeQuery()) {
-                return rs.next(); // Devuelve true si hay resultado
+                return rs.next(); // Devuelve true si hay resultados
             }
             
         } catch (SQLException es) {
-            muestraErrorSQL(es);
+            showSQLError(es);
         } catch (Exception e) {
             e.printStackTrace(System.err);
         }
@@ -294,7 +351,7 @@ public class UD03TareaEvaluativaEjercicio3 {
      * Imprime información detallada del error para ayudar a depurar
      * @param es Objeto SQLException con los detalles del error
      */
-    public static void muestraErrorSQL(SQLException es) {
+    public static void showSQLError(SQLException es) {
         System.err.println("SQL ERROR mensaje: " + es.getMessage());
         System.err.println("SQL Estado: " + es.getSQLState());
         System.err.println("SQL código específico: " + es.getErrorCode());
